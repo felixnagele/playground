8814f52eb3aed806f46c51a140f4e45e40cdc7c1
Felix Nagele <43892671+felixnagele@users.noreply.github.com>
2025-12-12 00:05:20 +0100

    Add Python project template with initial structure and configuration

---
+++ python/template/src/sub_folder_1/test1.py
@@ -0,0 +1,7 @@
+"""Module in sub_folder_1."""
+
+from src.utils import add
+
+
+def calculate(a: int, b: int) -> int:
+    return add(a, b)
---
+++ python/template/src/utils.py
@@ -0,0 +1,5 @@
+"""Shared utility functions."""
+
+
+def add(a: int, b: int) -> int:
+    return a + b
