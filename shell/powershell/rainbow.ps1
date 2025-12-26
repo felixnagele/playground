86e7f693d6d134be6e47e8a52a863795ea97df3f
Felix Nagele <43892671+felixnagele@users.noreply.github.com>
2025-12-27 00:07:38 +0100

    Fix foreground color casting in rainbow.ps1

--- shell/powershell/rainbow.ps1
+++ shell/powershell/rainbow.ps1
@@ -1,7 +1,7 @@
 Clear-Host
 while ($true) {
   0..15 | ForEach-Object {
-    Write-Host ("Color $_") -ForegroundColor $_
+    Write-Host ("Color $_") -ForegroundColor ([System.ConsoleColor]$_)
     Start-Sleep -Milliseconds 100
   }
 }
