84a0aa7884afbaf44486ed309a5d4fdadf5b6354
Felix Nagele <43892671+felixnagele@users.noreply.github.com>
2025-12-27 00:09:05 +0100

    Fix foreground color in spinner.ps1 to improve visibility

--- shell/powershell/spinner.ps1
+++ shell/powershell/spinner.ps1
@@ -2,7 +2,7 @@ Clear-Host
 $spin = @("|","/","-","\")
 while ($true) {
   foreach ($c in $spin) {
-    Write-Host -NoNewline "`r$c" -ForegroundColor DarkBlue
+    Write-Host -NoNewline "`r$c" -ForegroundColor Cyan
     Start-Sleep -Milliseconds 100
   }
 }
